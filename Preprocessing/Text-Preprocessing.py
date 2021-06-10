f4222b28ba79d2f1125598664f638538ab47f0fc
caraguisebastian <57133663+caraguisebastian@users.noreply.github.com>
2021-06-10 21:43:53 +0300

    Add average entities per sentence and average word length (syllables)

--- Preprocessing/Text-Preprocessing.py
+++ Preprocessing/Text-Preprocessing.py
@@ -18,6 +18,7 @@ print(len(corpus))
 
 #Preprocessing && statistics
 import spacy
+import pyphen
 
 lemmasDictionary = dict()
 tokensDictionary = dict()
@@ -29,12 +30,19 @@ numberOfSentences = 0
 numberOfEntities = 0
 avgWordLenChars = dict()
 avgSentenceLen = dict()
+avgEntsPerSent = dict()
+myPyphen = pyphen.Pyphen(lang='ro')
+avgWordLenSyl = dict()
 while i < 50:
     data = corpus[i]
     doc = nlp(data)
     for token in doc:
         if token.pos_ == 'PROPN':
             numberOfEntities += 1
+        if str(len(myPyphen.inserted(token.text).split('-'))) not in avgWordLenSyl:
+            avgWordLenSyl[str(len(myPyphen.inserted(token.text).split('-')))] = 1
+        else:
+            avgWordLenSyl[str(len(myPyphen.inserted(token.text).split('-')))] += 1
 
     data = data.lower()
     print(i)
@@ -74,6 +82,17 @@ while i < 50:
             avgSentenceLen[str(len(doc[currentSentence].sent))] = 1
         else:
             avgSentenceLen[str(len(doc[currentSentence].sent))] += 1
+        currentWord = currentSentence
+        entsInSent = 0
+
+        while currentWord < (currentSentence + len(doc[currentSentence].sent)):
+            if doc[currentWord].pos_ == 'PROPN':
+                entsInSent += 1
+            currentWord += 1
+        if str(entsInSent) not in avgEntsPerSent:
+            avgEntsPerSent[str(entsInSent)] = 1
+        else:
+            avgEntsPerSent[str(entsInSent)] += 1
         currentSentence += len(doc[currentSentence].sent)
     i+=1
 
@@ -106,8 +125,21 @@ for k, v in avgWordLenChars.items():
     print(k, end=' characters: ')
     print(v)
 
+print("Average word length in syllables: ")
+avgWordLenSyl = {k: v for k, v in sorted(avgWordLenSyl.items(), key=lambda item: item[1], reverse=True)}
+for k, v in avgWordLenSyl.items():
+    print(k, end=' syllables: ')
+    print(v)
+
 print("Average sentence length: ")
 avgSentenceLen = {k: v for k, v in sorted(avgSentenceLen.items(), key=lambda item: item[1], reverse=True)}
 for k, v in avgSentenceLen.items():
     print(k, end=' words: ')
     print(v)
+
+print("Average entities per sentence: ")
+avgEntsPerSent = {k: v for k, v in sorted(avgEntsPerSent.items(), key=lambda item: item[1], reverse=True)}
+for k, v in avgEntsPerSent.items():
+    print(k, end=' entities: ')
+    print(v)
+
